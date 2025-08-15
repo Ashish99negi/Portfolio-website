2d1c865084dd73c478306c76b5dca396dcd48f02
Ashish Negi <negi99ashish@gmail.com>
2025-08-15 23:37:31 +0530

    profile picture added.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -18,12 +18,20 @@ export class ProjectsComponent {
       status: 'complete'
     },
     {
+      title: 'Online Resume Builder',
+      description: 'An online resume builder developed with Angular, allowing users to easily create, edit, and preview their resumes before downloading a PDF.',
+      link: 'https://resume-builder-61ee.vercel.app/resume-editor',
+      imageUrl: 'assets/images/online-resume-builder.png',
+      status: 'complete'
+    },
+    {
       title: 'Form Builder',
       description: 'An interactive form builder built with Angular 17, Tailwind CSS, and Angular Material. This platform features a modern drag-and-drop canvas for visually designing custom forms. It includes a powerful export tool that generates ready-to-use, HTML code for seamless integration into any Angular application..',
       link: 'https://online-form-builder.vercel.app/',
       imageUrl: 'assets/images/form-builder.png',
       status: 'complete'
     },
+    
     {
       title: 'Journey Planner App',
       description: 'A responsive and interactive Journey Planner built with Angular 17. This application allows users to create dynamic, multi-stop routes, discover nearby hotels and restaurants, view destination photo galleries for their trip.',
