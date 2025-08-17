ff2f70f0ea9a7c7ad4fc3dcbbf86d9d93c6a4cb4
Ashish Negi <negi99ashish@gmail.com>
2025-08-18 01:18:02 +0530

    change layout, and update portfoio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'skills', component: SkillsComponent },
+ { path: 'about', component: ProfileComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: '**', redirectTo: '' }
 ];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,16 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  skills = [
+    'HTML', 'CSS', 'JavaScript', 'TypeScript', 'Angular', 'Tailwind CSS', 'Bootstrap', 'RxJS', 'Git', 'REST APIs', 'Postman', 'JIRA', 'Jasmine', 'Karma'
+  ];
 
 }
